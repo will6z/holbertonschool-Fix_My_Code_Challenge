b3dbff4f3dce38f3fb485c947c7870eb85bc32f9
William <will@penguin.com>
2024-09-26 15:36:01 -0400

    2

--- challenge/2-sort.rb
+++ challenge/2-sort.rb
@@ -1,30 +1,30 @@
 ###
 #
-#  Sort numeric arguments (ascending) 
+#  Sort integer arguments (ascending) 
 #
 ###
 
 result = []
 ARGV.each do |arg|
-    # skip if not numeric
-    next if arg !~ /^-?[0-9]+(\.[0-9]+)?$/
+    # skip if not integer
+    next if arg !~ /^-?[0-9]+$/
 
-    # convert to float
-    f_arg = arg.to_f
+    # convert to integer
+    i_arg = arg.to_i
     
     # insert result at the right position
     is_inserted = false
     i = 0
     l = result.size
     while !is_inserted && i < l do
-        if result[i] > f_arg
-            result.insert(i, f_arg)
+        if result[i] > i_arg
+            result.insert(i, i_arg)
             is_inserted = true
             break
         end
         i += 1
     end
-    result << f_arg if !is_inserted
+    result << i_arg if !is_inserted
 end
 
 puts result
